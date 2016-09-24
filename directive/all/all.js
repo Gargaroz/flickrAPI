47315be4bedc04f4bfedbbbfa954d1c0d88fcdf6
Gianluca Esposito <g.esposito16@gmail.com>
2016-09-24 21:55:10 +0200

    Pagination
    added: pagination to the "related images" page
    fixed: related images responsive classes

--- directive/all/all.js
+++ directive/all/all.js
@@ -7,13 +7,22 @@ angular.module('flickrAPI').directive('all', function() {
         controller: function($scope, selectedPhoto, flickrApi) {
           var vm = this;
           vm.allPhotos = [];
+          vm.displayedPhotos = [];
+          vm.totalItems = 0;
+          vm.currentPage = 1;
+          vm.maxItems = 18;
           var getPhotos = function() {
             flickrApi.getFlickrBatch(selectedPhoto.getTag(), selectedPhoto.getUser()).then(function(response){
               vm.allPhotos = response.photos.photo;
-              vm.displayedPhotos = vm.allPhotos.slice(0,vm.maxItems);
               vm.currentPage = response.photos.page; 
               vm.totalItems = response.photos.photo.length;
+              vm.stepPage();
             });
+          };
+          vm.stepPage = function() {
+            vm.displayedPhotos = vm.allPhotos.slice(vm.maxItems * (vm.currentPage - 1), vm.maxItems * vm.currentPage);
+            vm.displayedPhotos;
+            
           };
           vm.$onInit = function () {
             getPhotos();
